4a863583a9afa9eaf6c773aea52ff572c81296ef
quietkatstudio <quietkat.studio@gmail.com>
2025-11-22 23:35:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const apiKey = "88e03081dda30cb855fbfb0482c4bc2f";
 
+document.getElementById("getWeatherBtn").addEventListener("click", () => {
+  const city = document.getElementId("cityInput").value;
+  if(city) {
+    getWeather(city);
+  }
+});
+
+function getWeather(city) {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appic=${apiKey}')
+  .then(response => response.json())
+  .then(data => {
+    if(data.cod == 200) {
+      const weatherInfo = '
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <p>Temperature: ${data.main.temp} °C</p>
+        <p>Weather: ${data.weather[0].description}</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        ';
+      document.getElementById("weatherResult").innerHTML = weatherInfo;
+    } else {
+        document.getElementById("weatherResult").innerHTML = '<p>City not found</p>';
+    }
+
+  })
+  .catch(error => {
+     document.getElementById("weatherResult").innerHTML = '<p>Error fetching data</p>';
+    console.error(error);
+  });
+}
