a82145ba73cfbec9e83f0e3e013413e2e0a6694c
quietkatstudio <quietkat.studio@gmail.com>
2025-11-23 00:00:13 -0500

    Update script.js made a delay until page is loaded

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-const apiKey = "88e03081dda30cb855fbfb0482c4bc2f";
+document.addEventListener("DOMContentLoaded", () => {
+  const apiKey = "88e03081dda30cb855fbfb0482c4bc2f";
 
-document.getElementById("getWeatherBtn").addEventListener("click", () => {
-  const city = document.getElementById("cityInput").value;
-  if(city) {
-    getWeather(city);
+  document.getElementById("getWeatherBtn").addEventListener("click", () => {
+    const city = document.getElementById("cityInput").value;
+    if(city) {
+      getWeather(city);
+    }
+  });
+
+  function getWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      if(data.cod == 200) {
+        const weatherInfo = `
+          <h2>${data.name}, ${data.sys.country}</h2>
+          <p>Temperature: ${data.main.temp} °C</p>
+          <p>Weather: ${data.weather[0].description}</p>
+          <p>Humidity: ${data.main.humidity}%</p>
+          `;
+        document.getElementById("weatherResult").innerHTML = weatherInfo;
+      } else {
+          document.getElementById("weatherResult").innerHTML = `<p>City not found</p>`;
+      }
+
+    })
+    .catch(error => {
+      document.getElementById("weatherResult").innerHTML = `<p>Error fetching data</p>`;
+      console.error(error);
+    });
   }
 });
-
-function getWeather(city) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    if(data.cod == 200) {
-      const weatherInfo = `
-        <h2>${data.name}, ${data.sys.country}</h2>
-        <p>Temperature: ${data.main.temp} °C</p>
-        <p>Weather: ${data.weather[0].description}</p>
-        <p>Humidity: ${data.main.humidity}%</p>
-        `;
-      document.getElementById("weatherResult").innerHTML = weatherInfo;
-    } else {
-        document.getElementById("weatherResult").innerHTML = `<p>City not found</p>`;
-    }
-
-  })
-  .catch(error => {
-     document.getElementById("weatherResult").innerHTML = `<p>Error fetching data</p>`;
-    console.error(error);
-  });
-}
